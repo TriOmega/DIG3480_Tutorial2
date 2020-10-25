18f0fb7e14746bd69bd64eaf065abf7858ea3c45
Eric Osorio <ericdanielosorio@gmail.com>
2020-10-25 18:39:41 -0400

    First Challenge Commit

--- Assets/Scripts/CameraScript.cs
+++ Assets/Scripts/CameraScript.cs
@@ -11,9 +11,15 @@ public class CameraScript : MonoBehaviour
         
     }
 
-    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey("escape"))
+        {
+            Application.Quit();
+        }
+    }
     void LateUpdate()
     {
-        this.transform.position = new Vector3(target.transform.position.x, this.transform.position.y, this.transform.position.z);
+        this.transform.position = new Vector3(target.transform.position.x, target.transform.position.y, this.transform.position.z);
     }
 }
--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -7,21 +7,46 @@ public class PlayerScript : MonoBehaviour
 {
     private Rigidbody2D rd2d;
     public float speed;
-    public Text score;
+    public Text scoreText;
+    public Text winText;
+    public Text livesText;
     private int scoreValue = 0;
+    private int livesValue = 3;
+    private bool isOnGround = false;
+    public Transform groundcheck;
+    public float checkRadius;
+    public LayerMask allGround;
+    public AudioSource musicSource;
+    public AudioClip musicGame;
+    public AudioClip musicWin;
+    Animator anim;
+    private bool facingRight = true;
     // Start is called before the first frame update
     void Start()
     {
         rd2d = GetComponent<Rigidbody2D>();
-        score.text = scoreValue.ToString();
+        anim = GetComponent<Animator>();
+        scoreText.text = "Score: " + scoreValue.ToString();
+        winText.text = "";
+        livesText.text = "Lives: " + livesValue.ToString();
+        musicSource.clip = musicGame;
+        musicSource.Play();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKey("escape"))
+        if(Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D) || Input.GetKey("left") || Input.GetKey("right"))
         {
-            Application.Quit();
+            anim.SetInteger("State", 1);
+        }
+        else
+        {
+            anim.SetInteger("State", 0);
+        }
+        if(isOnGround == false)
+        {
+            anim.SetInteger("State", 2);
         }
     }
 
@@ -31,6 +56,17 @@ public class PlayerScript : MonoBehaviour
         float verMovement = Input.GetAxis("Vertical");
 
         rd2d.AddForce(new Vector2(hozMovement * speed, verMovement * speed));
+
+        isOnGround = Physics2D.OverlapCircle(groundcheck.position, checkRadius, allGround);
+
+        if(facingRight == false && hozMovement > 0)
+        {
+            Flip();
+        }
+        else if(facingRight == true && hozMovement < 0)
+        {
+            Flip();
+        }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -38,19 +74,58 @@ public class PlayerScript : MonoBehaviour
         if(collision.collider.tag == "Coin")
         {
             scoreValue += 1;
-            score.text = scoreValue.ToString();
+            scoreText.text = "Score: " + scoreValue.ToString();
             Destroy(collision.collider.gameObject);
+            if(scoreValue == 4)
+            {
+                transform.position = new Vector3(43.0f,6.5f,0.0f);
+                livesValue = 3;
+                SetLivesText();
+            }
+            if(scoreValue == 8)
+            {
+                winText.text = "You Win!\nGame created by: Eric Osorio";
+                musicSource.clip = musicWin;
+                musicSource.Play();
+            }
+        }
+        if(collision.collider.tag == "Enemy")
+        {
+            livesValue -= 1;
+            SetLivesText();
+            Destroy(collision.collider.gameObject);
+            if(livesValue == 0)
+            {
+                winText.text = "You Lose...";
+                Destroy(this);
+            }
+        }
+        if(collision.collider.tag == "BadFloor")
+        {
+            transform.position = new Vector3(43.0f,6.5f,0.0f);
         }
     }
     
     private void OnCollisionStay2D(Collision2D collision)
     {
-        if(collision.collider.tag == "Ground")
+        if(collision.collider.tag == "Ground" && isOnGround)
         {
             if((Input.GetKey(KeyCode.W)) || (Input.GetKey("up")))
             {
                 rd2d.AddForce(new Vector2(0, 3), ForceMode2D.Impulse);
             }
         }
+    }
+
+    private void SetLivesText()
+    {
+        livesText.text = "Lives: " + livesValue.ToString();
+    }
+    private void Flip()
+    {
+        facingRight = !facingRight;
+        Vector2 Scaler = transform.localScale;
+        Scaler.x = Scaler.x * -1;
+        transform.localScale = Scaler;
     }
 }
